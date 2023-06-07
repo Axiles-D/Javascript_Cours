af9bd0396e26ecf3dc70eaf6cc31ff9004a2df40
Alexis <delattre.alexis02680@gmail.com>
2023-06-07 18:26:31 +0200

    Avancé sur le js du pendu

--- pendu.js
+++ pendu.js
@@ -1,20 +1,43 @@
 const button = document.getElementsByTagName('button');
 const tabbleMot = ["manga", "developpeur", "anticonstitutionnellement", "kinesitherapeute", "ballon", "code", "grandir", "peripateticienne"]
 const motRandom = Math.floor(Math.random() * tabbleMot.length);
-const mot = [tabbleMot[motRandom]];
+const mot = tabbleMot[motRandom];
+const caracter = mot.split("");
 const etoile = [""];
 const guessWord = document.querySelector("#guessWord");
+let iswin = 0
+let islose = 0
+
 
 console.log(mot);
-for (let i = 0; i < mot[0].length; i++) {
+
+//boucle qui affiche les étoiles en fonction de la longueur du mot
+for (let i = 0; i < mot.length; i++) {
     etoile[0] += "* "
 }
+
 console.log(etoile);
+
+console.log(caracter);
+
 guessWord.textContent = `${etoile}`;
+
+
+// Fonction qui analyse la réponse donnée
 
 for (let i = 0; i < button.length; i++) {
     button[i].addEventListener("click", function() {
-        if (button[i] // créer un tableau avec toutes les lettres de l'alphabet pour comparer avec le contenu des mots à guess
+        if (caracter.includes(button[i].textContent.toLowerCase())) {
+            // remplacer l'étoile par la lettre qui correspond
+            if (iswin == mot.length) {
+                // Condition de victoire
+            }
+        } else {
+            islose++;
+        }
+        if (islose == 7) {
+            //Condition de perdu
+        }
     button[i].disabled = true;
     });
 }
